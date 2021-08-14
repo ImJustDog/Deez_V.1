fdc66534374d7b1e7f26b38c747a25d128301876
ImJustDog <87216585+ImJustDog@users.noreply.github.com>
2021-08-14 11:15:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ class MyClient(discord.Client):
 
         if message.content == 'คนสร้าง':
             await message.channel.send('Im Just Dog#6918')
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
         if message.content == 'coding':
             await message.channel.send('Im Just Dog#6918')
             
