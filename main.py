b5c6f5fc2b4dbca542b8c75c4c8d1294facacc3d
ImJustDog <87216585+ImJustDog@users.noreply.github.com>
2021-08-14 11:13:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import discord
+from discord.ext import commands
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'คนสร้าง':
+            await message.channel.send('Im Just Dog#6918')
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'coding':
+            await message.channel.send('Im Just Dog#6918')
+            
+
+client = MyClient()
+client.run('') # Token of bot #
+
+client.run(bottoken)
